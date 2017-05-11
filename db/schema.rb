11bbda4918c78b474004ff14961f657fcb587dd0
Peter Strong <peter.strong@gmail.com>
2017-05-11 19:59:18 -0400

    create model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502004637) do
+ActiveRecord::Schema.define(version: 20170511233453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -157,6 +157,17 @@ ActiveRecord::Schema.define(version: 20170502004637) do
     t.datetime "end_date"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "workshops", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "affiliation"
+    t.string   "occupation"
+    t.string   "email"
+    t.string   "phone"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
