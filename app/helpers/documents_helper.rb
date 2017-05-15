75a9ac7fa1084a0e9d5b9f6c32bc393914ac49b7
Michael Yatco <mjyatco@gmail.com>
2017-05-15 19:46:07 -0400

    assigned appropriate values for documents controller

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,8 +1,8 @@
 module DocumentsHelper
 
   def document_lists
-    current_user.international_applicant? ? documents = [['health form'], ['personal essay'], ['recommendation'], ['resume'], ['TOEFL/IELTS score report'], ['transcript'], ['WES evaluation'], ['passport_photo']] :
-      documents = [['health form'], ['personal essay'], ['recommendation'], ['resume'], ['transcript']]  
+    current_user.international_applicant? ? [['Health Form', 'health_form'], ['Personal Essay', 'personal_essay'], ['Recommendation', 'recommendation'], ['Resumé', 'resume'], ['TOEFL/IELTS Score Report', 'toefl_ielts_score_report'], ['Transcript', 'transcript'], ['WES Evaluation', 'wes_evaluation'], ['Contract', 'contract'], ['Student Visa', 'student_visa'], ['Passport Photo', 'passport_photo']] :
+      [['Health Form', 'health_form'], ['Personal Essay', 'personal_essay'], ['Recommendation', 'recommendation'], ['Resumé', 'resume'], ['Contract', 'contract'], ['Transcript', 'transcript']]  
   end
 
 end
