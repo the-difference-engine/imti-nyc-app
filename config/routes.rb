8e1d2a1ba5dcdb2a6e4fef42b402c52856b71df7
Michael Yatco <mjyatco@gmail.com>
2017-05-15 20:06:46 -0400

    Removed unnecessary code

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,7 @@ Rails.application.routes.draw do
   resources :school_profiles, only: [:index, :new, :create, :edit, :update] do
   end
 
-  resources :local_schools, only: [:index, :new, :create, :edit, :update] do
-  end
+  resources :local_schools, only: [:index, :new, :create, :edit, :update] 
 
   resources :workshops
   resources :charges
