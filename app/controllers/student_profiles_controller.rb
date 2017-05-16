4f5a6d7f294698913156c76fa6b3ad22256d4d79
James Chou <jameschou1993@gmail.com>
2017-05-15 21:00:39 -0400

    used strong params in student profile controller

--- app/controllers/student_profiles_controller.rb
+++ app/controllers/student_profiles_controller.rb
@@ -5,7 +5,14 @@ class StudentProfilesController < ApplicationController
   end
 
   def update
-    current_user.update()
+    student = current_user
+    application = current_user.application
+    if student.update(user_params) && application.update(application_params)
+      flash[:success] = "Student Profile Successfully Update"
+      redirect_to '/student_profiles'
+    else
+      flash[:danger] = student.errors.full_messages 
+    end
   end
 
   def new
@@ -19,6 +26,16 @@ class StudentProfilesController < ApplicationController
   def index
     @title = "Profile"
     @student = current_user
+  end
+
+  private 
+
+  def user_params
+    params.permit(:first_name, :middle_initial, :last_name)
+  end
+
+  def application_params
+    params.permit( :street, :city, :state, :zip_code)
   end
 
 end
