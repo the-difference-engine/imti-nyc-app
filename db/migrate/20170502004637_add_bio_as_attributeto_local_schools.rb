2a59c91fb945201cb7bf3f310750d0de7381994d
James Chou <jameschou1993@gmail.com>
2017-05-15 20:02:48 -0400

    removed unessecary info in new app form, created student_profile controller, and created student profile view

--- db/migrate/20170502004637_add_bio_as_attributeto_local_schools.rb
+++ db/migrate/20170502004637_add_bio_as_attributeto_local_schools.rb
@@ -1,4 +1,4 @@
-class AddBioAsAttributetoLocalSchools < ActiveRecord::Migration[5.0]
+kclass AddBioAsAttributetoLocalSchools < ActiveRecord::Migration[5.0]
   def change
     add_column :local_schools, :bio, :text
   end
