7324aeacbd40b1263af7c5f5ba8748ccd2260acc
Peter Strong <peter.strong@gmail.com>
2017-05-15 21:02:01 -0400

    update registrants controller and views

--- app/controllers/registrants_controller.rb
+++ app/controllers/registrants_controller.rb
@@ -1,9 +1,5 @@
 class RegistrantsController < ApplicationController
-  def index
-    @registrants = Registrant.all
-    render 'index.html.erb'
-  end
-
+  
   def new
     @registrant = Registrant.new
   end
@@ -11,24 +7,12 @@ class RegistrantsController < ApplicationController
   def create
     @registrant = Registrant.new(first_name: params[:first_name], last_name: params[:last_name], affiliation: params[:affiliation], occupation: params[:occupation],  email: params[:email])
     @registrant.save 
-  end
-
-  def show
-    @registrant = Registrant.find_by(id: params[:id])
-    render 'show.html.erb'    
+    redirect_to "/workshops"
   end
 
   def edit 
     @registrant = Registrant.find_by(id: params[:id])
+    @registrant.update(first_name: params[:first_name], last_name: params[:last_name], affiliation: params[:affiliation], occupation: params[:occupation],  email: params[:email])
     render 'edit.html.erb'
-  end
-
-  def update
-    @registrant = Registrant.find_by(id: params[:id])
-    @registrant.assign_attributes()
-  end
-
-  def destroy 
-    
   end
 end
