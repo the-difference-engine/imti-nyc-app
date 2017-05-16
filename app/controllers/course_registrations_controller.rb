e0d2aabfc0a7a19cfc5c5560d4227915479dfbc4
David Klaver <davidjklaver@gmail.com>
2017-05-15 20:45:56 -0400

    insert custom stripe form into new course registrations page

--- app/controllers/course_registrations_controller.rb
+++ app/controllers/course_registrations_controller.rb
@@ -2,18 +2,34 @@ class CourseRegistrationsController < ApplicationController
 
 	def new
 		@courses = Course.all
-		@course_registration = CourseRegistration.new
 	end
 
 	def create
-		course_registration = CourseRegistration.new(course_registration_params)
 		course = Course.find(course_registration_params[:course_id])
-		@amount = course.price
-		@product_name = course.name
-		render 'charges/new.html.erb'
+		amount = (course.price * 100).to_i
+    customer = Stripe::Customer.create(
+      :email => current_user.email,
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => amount,
+      :description => course.name,
+      :currency    => 'usd'
+    )
+
+    @new_charge = current_user.charges.create(uid: charge.id, amount: charge.amount / 100, description: charge.description, customer_id: customer.id)
+    current_user.update(course_id: course.id)
+    # redirect_to charge_path(new_charge.id)
+    render "/charges/show.html.erb"
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to "/course_registrations/new"
 	end
 
 	def course_registration_params
-		params.require(:course_registration).permit(:course_id).merge(user_id: current_user.id)
+		params.require(:course).permit(:course_id).merge(user_id: current_user.id)
 	end
 end
