a2af4f363342ed494b4acff57814d0112d726428
Dan <dannguyen90@gmail.com>
2017-05-18 19:30:23 -0400

    removed unnecessary code and typo

--- db/migrate/20170502004637_add_bio_as_attributeto_local_schools.rb
+++ db/migrate/20170502004637_add_bio_as_attributeto_local_schools.rb
@@ -1,4 +1,4 @@
-kclass AddBioAsAttributetoLocalSchools < ActiveRecord::Migration[5.0]
+class AddBioAsAttributetoLocalSchools < ActiveRecord::Migration[5.0]
   def change
     add_column :local_schools, :bio, :text
   end
