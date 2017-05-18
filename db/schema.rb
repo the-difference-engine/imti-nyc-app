197138f01822906d27dd5473cff3f121da92f6f6
James Chou <jameschou1993@gmail.com>
2017-05-18 19:51:09 -0400

    remove avatar and principal_name

--- db/schema.rb
+++ db/schema.rb
@@ -96,7 +96,6 @@ ActiveRecord::Schema.define(version: 20170515231313) do
     t.string   "name"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.string   "principal_name"
     t.string   "contact_name"
     t.string   "school_address"
     t.text     "bio"
@@ -157,10 +156,6 @@ ActiveRecord::Schema.define(version: 20170515231313) do
     t.boolean  "donor"
     t.integer  "role"
     t.integer  "local_school_id"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
