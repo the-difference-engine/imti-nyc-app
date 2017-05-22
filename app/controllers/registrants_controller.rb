e17a36ab17335c44f193d3e430828280ddb2b3f5
Peter Strong <peter.strong@gmail.com>
2017-05-22 09:55:58 -0400

    update workshop registration for individuals

--- app/controllers/registrants_controller.rb
+++ app/controllers/registrants_controller.rb
@@ -1,12 +1,14 @@
 class RegistrantsController < ApplicationController
-  
+  skip_before_action :authenticate_user!
+
   def new
     @registrant = Registrant.new
   end
 
   def create
-    @registrant = Registrant.new(first_name: params[:first_name], last_name: params[:last_name], affiliation: params[:affiliation], occupation: params[:occupation],  email: params[:email])
-    @registrant.save 
+    registrant = Registrant.new(first_name: params[:first_name], last_name: params[:last_name], affiliation: params[:affiliation], occupation: params[:occupation],  email: params[:email], phone: params[:phone])
+    registrant.save 
+    RegistrantWorkshop.create(registrant_id: registrant.id, workshop_id: params[:workshop_id])
     redirect_to "/workshops"
   end
 
