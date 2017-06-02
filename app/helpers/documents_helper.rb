84f416c146d9957f6d53153bb3610d78ce65a7ed
Michael Yatco <mjyatco@gmail.com>
2017-06-01 20:57:17 -0400

    DocumentsHelper expanded/Mailgun activated

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -5,4 +5,33 @@ module DocumentsHelper
       [['Health Form', 'health_form'], ['Personal Essay', 'personal_essay'], ['Recommendation', 'recommendation'], ['Resumé', 'resume'], ['Contract', 'contract'], ['Transcript', 'transcript']]  
   end
 
+  def send_email_mailgun
+    mg_client = Mailgun::Client.new(ENV["MAILGUN_API_KEY"])
+
+    message_params =  {
+                       from: 'imtinyc@gmail.com',
+                       to:   'imtinyc@gmail.com',
+                       subject: 'All Documents Uploaded Successfully!',
+                       text:    'You did it! You are now ready to pay the application fee'
+                      }
+
+    result = mg_client.send_message('sandbox4f9920610a894b81b82f6bc37e90f1a0.mailgun.org', message_params).to_h!
+
+    message_id = result['id']
+    message = result['message']
+  end 
+
+  def finish_domestic_documents(application)
+    list_of_domestic_documents = ['health_form', 'personal_essay', 'recommendation', 'resume', 'contract', 'transcript'] 
+
+    return false if application.documents.count != list_of_domestic_documents.size
+
+    current_documents = []
+    application.documents.each do |document|
+      current_documents << document.category
+    end 
+
+    current_documents.sort == list_of_domestic_documents.sort
+  end 
+
 end
