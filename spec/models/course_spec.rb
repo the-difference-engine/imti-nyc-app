756b5d1795355c9cf0f46251dcae92170fee8b1f
Andrew <andrewsouthard111@gmail.com>
2017-06-15 19:30:05 -0400

    Created course rspec file and factory for course

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "has valid factory" do
+    expect(FactoryGirl.create(:course)).to be_valid
+  end
+
+  it "price is a decimal" do
+    course = FactoryGirl.create(:course)
+    expect(course.price.class).to eq(BigDecimal)
+  end
 end
