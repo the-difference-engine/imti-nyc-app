97207979d584f56ce9d78cf198f209fcda1e6f06
Andrew <andrewsouthard111@gmail.com>
2017-06-15 19:30:05 -0400

    Created course rspec file and factory for course

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
   it "has valid factory" do
     expect(FactoryGirl.create(:course)).to be_valid
   end
