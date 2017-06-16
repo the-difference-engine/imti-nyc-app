a5d870e644774c2b6e917c06ff938f7345b537b3
Andrew <andrewsouthard111@gmail.com>
2017-06-15 20:57:46 -0400

    Add user association test, delete course_registration model

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,4 +1,12 @@
 FactoryGirl.define do
+  factory :user do 
+    first_name "John"
+     last_name "Smith"
+     email "js@email.com"
+     # course_id course.id
+     password "password"
+  end 
+
   factory :course do
     name "Summer"
     start_date "04/04/2017"
