1afb89625a9e1d95be67a563f2d3373e5d64fd9d
Josh <jspringxc@gmail.com>
2017-06-15 20:50:34 -0400

    added rspec test for Education model.  Created educations factory

--- spec/models/education_spec.rb
+++ spec/models/education_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Education, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:education)).to be_valid
+  end
+
+  describe "converts date to strftime" do
+    education = FactoryGirl.build(:education)
+    it 'should convert education start date to strftime' do
+      expect(education.pretty_date(education.start_date)).to eq '01/01/2001'
+    end
+  end
+
+
 end
