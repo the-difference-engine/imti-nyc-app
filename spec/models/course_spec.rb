b41eab39f59262d7ea822701b66e84d5b07ad977
Andrew <andrewsouthard111@gmail.com>
2017-06-15 20:57:46 -0400

    Add user association test, delete course_registration model

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,19 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Course, type: :model do
   it "has many users" do
-    course = FactoryGirl.create(:course)
-    john = User.create(
-      first_name: "John",
-      last_name: "Smith",
-      email: "js@email.com",
-      course_id: course.id
-    )
-    jeff = User.create(
-      first_name: "Jeff",
-      last_name: "Sessions",
-      email: "jjj@email.com",
-      course_id: course.id
-    )
+    course = FactoryGirl.build(:course)
+    john = FactoryGirl.build(:user)
+    tom = FactoryGirl.build(:user, first_name: "tom")
+
+    course.users << john << tom 
+
+    # p "course #{course.id}"
+    # john = User.create!(
+    #   first_name: "John",
+    #   last_name: "Smith",
+    #   email: "js@email.com",
+    #   course_id: course.id,
+    #   password: "password",
+    #   password_confirmation: "password"
+    # )
+    # jeff = User.create!(
+    #   first_name: "Jeff",
+    #   last_name: "Sessions",
+    #   email: "jjj@email.com",
+    #   course_id: course.id,
+    #   password: "password",
+    #   password_confirmation: "password"
+    # )
+    # p "course.users #{course.users}"
+    # p "course.users #{course.users[0]}"
     expect(course.users[0].id).to eq(john.id)
   end
 
