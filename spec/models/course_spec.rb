50c5badd83337c939542474e6f91270d9e9751d3
Andrew <andrewsouthard111@gmail.com>
2017-06-15 20:02:06 -0400

    Wrote test for associations with user

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe Course, type: :model do
     )
     expect(course.users[0].id).to eq(john.id)
   end
+
+  it "has many users" do
+    course = FactoryGirl.create(:course)
+    john = User.create(
+      first_name: "John",
+      last_name: "Smith",
+      email: "js@email.com",
+      course_id: course.id
+    )
+    jeff = User.create(
+      first_name: "Jeff",
+      last_name: "Sessions",
+      email: "jjj@email.com",
+      course_id: course.id
+    )
+    expect(course.users[0].id).to eq(john.id)
+  end
 end
