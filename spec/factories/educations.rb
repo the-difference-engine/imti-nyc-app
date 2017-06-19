7aba272ad466020b7f64bd9a0070862f81b0b10b
Jamie MacKillop <jamieronin@gmail.com>
2017-06-18 20:47:18 -0400

    Add factorygirl syntax to spec/support/factory_girl.rb. Require support/factorygirl.rb to rails_helper.rb. Add user, education, application factories to factories/educations.rb. Add first factory validation test to education_spec.rb

--- spec/factories/educations.rb
+++ spec/factories/educations.rb
@@ -1,5 +1,11 @@
 FactoryGirl.define do
   factory :education do
-    
+    association :application
+    school "My School"
+    location "No where"
+    start_date "2017-06-8 19:36:36"
+    end_date "2017-06-10 19:36:36"
+    degree "Being so rad"
+    degree_date "2017-06-11 19:36:36"
   end
 end
