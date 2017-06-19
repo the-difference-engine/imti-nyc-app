fd5e50f81d38e23b9bb5958c7858646819127214
Josh <jspringxc@gmail.com>
2017-06-19 19:05:09 -0400

    add validation tests

--- spec/models/education_spec.rb
+++ spec/models/education_spec.rb
@@ -11,7 +11,48 @@ RSpec.describe Education, type: :model do
     it 'should convert education start date to strftime' do
       expect(education.pretty_date(education.start_date)).to eq '01/01/2001'
     end
+
+    it 'should convert education end date to strftime' do 
+      expect(education.pretty_date(education.end_date)).to eq '05/30/2008'
+    end
   end
 
+  describe "test validations" do
+    it "should return error if start date is nil" do
+      education = FactoryGirl.build(:education, start_date: nil)
+      education.valid?
+      expect(education.errors[:start_date]).to include("can't be blank")
+    end
+
+    it "should return error if school is nil" do
+      education = FactoryGirl.build(:education, school: nil)
+      education.valid?
+      expect(education.errors[:school]).to include("can't be blank")
+    end
+
+    it "should return error if location is nil" do
+      education = FactoryGirl.build(:education, location: nil)
+      education.valid?
+      expect(education.errors[:location]).to include("can't be blank")
+    end
+
+    it "should return error if end date is nil" do
+      education = FactoryGirl.build(:education, end_date: nil)
+      education.valid?
+      expect(education.errors[:end_date]).to include("can't be blank")
+    end
+
+    it "should return error if degree is nil" do
+      education = FactoryGirl.build(:education, degree: nil)
+      education.valid?
+      expect(education.errors[:degree]).to include("can't be blank")
+    end
+
+    it "should return error if degree date is nil" do
+      education = FactoryGirl.build(:education, degree_date: nil)
+      education.valid?
+      expect(education.errors[:degree_date]).to include("can't be blank")
+    end
+  end
 
 end
