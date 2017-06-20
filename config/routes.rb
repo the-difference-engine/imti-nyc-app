f8e23858044e8aab664211eea00e8b91737fe1ad
Eric Fithian <ef2413@columbia.edu>
2017-06-19 20:40:04 -0400

    Created payment modal for applications

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "/template" => 'pages#template'
 
   resources :applications, only: [:index, :new, :show, :create, :edit, :update] do
+    put :payment
   	resources :educations, only: [:index, :new, :create, :edit, :update]
   	resources :montessori_trainings, only: [:index, :new, :create, :edit, :update]
   	resources :work_experiences, only: [:index, :new, :create, :edit, :update]
