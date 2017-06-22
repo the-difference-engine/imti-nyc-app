fb3fd494a198e27e6e5a7f90016d5efdc52b348f
Jamie MacKillop <jamieronin@gmail.com>
2017-06-22 14:41:05 -0400

    Add controller test to Educaitons controller. Can populate array of all educations. Can render an index page.

--- spec/controllers/educations_controller_spec.rb
+++ spec/controllers/educations_controller_spec.rb
@@ -1,20 +1,27 @@
 require 'rails_helper'
-
+require 'pry'
 RSpec.describe EducationsController, type: :controller do
 
   describe 'GET #index' do
-    # it "populates an array of all educations" do
-    #   monte = create(:education, school: 'Monte')
-    #   ssori = create(:education, school: 'ssori')
-    #   get :index
-    #   expect(assigns(:education)).to match_array([monte, ssori])
-    # end
- 
-    it "renders the :index template" do
-
-      get :index
-      expect(response).to render_template :index
+    before :each do
+      @app = create(:application, id: 1)
+      @user = sign_in create(:user)
     end
 
+  context "signed in user"
+    it "can populate an array of all educations" do
+      monte = create(:education, school: 'Monte', application_id: @app.id)
+      sorri = create(:education, school: "Sorri", application_id: @app.id)
+      school = create(:education, school: "the school", application_id: @app.id)
+
+      get :index, {:application_id => @app, :user_id => @user}
+      expect(assigns(:educations)).to match_array([monte, sorri, school])
+    end
+
+  context "signed in user"
+    it "can render the :index template" do
+      get :index, {:application_id => @app.id}
+      expect(response).to render_template :index
+    end
   end
 end
