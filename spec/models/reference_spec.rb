b12c9ca90e92ffb44c682b5eddcc2220efedf4a0
Dov Bluming <dovybluming@gmail.com>
2017-06-22 19:37:58 -0400

    finished test model

--- spec/models/reference_spec.rb
+++ spec/models/reference_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Reference, type: :model do
-  describe Reference do
-    it "is valid with name address position" do
-      expect(build(:reference)).to be_valid
-    end
+describe Reference do
+  it 'is valid with name address position' do
+    expect(build(:reference)).to be_valid
   end
 end
