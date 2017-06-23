32ec9534148faf52a3cfd9d347dcbb2dc3efec95
Dov Bluming <dovybluming@gmail.com>
2017-06-22 21:09:28 -0400

    test code and fatory for work experience model

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,10 +1,12 @@
 class WorkExperience < ApplicationRecord
-	# validates :employer, :occupation, :start_date, :end_date, presence: true
+	validates :employer, :occupation, :start_date, :end_date, presence: true
   belongs_to :application
 
-  def pretty_date(date)
-    if date
-      date.strftime("%m/%d/%Y")
-    end
+  def pretty_start_date
+    start_date.strftime("%m/%d/%Y")
+  end
+
+  def pretty_end_date
+    end_date.strftime("%m/%d/%Y")
   end
 end
--- spec/models/work_experience_spec.rb
+++ spec/models/work_experience_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe WorkExperience, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe WorkExperience do
+  it 'is valid with employer, occupation, start date, end date' do
+    expect(build(:work_experience)).to be_valid
+  end
+  it 'is invalid without employer' do
+    work_experience = build(:work_experience, employer: nil)
+    work_experience.valid?
+    expect(work_experience.errors[:employer]).to include("can't be blank")
+  end
+  it 'is invalid without occupation' do
+    work_experience = build(:work_experience, occupation: nil)
+    work_experience.valid?
+    expect(work_experience.errors[:occupation]).to include("can't be blank")
+  end
+  it 'is invalid without start date' do
+    work_experience = build(:work_experience, start_date: nil)
+    work_experience.valid?
+    expect(work_experience.errors[:start_date]).to include("can't be blank")
+  end
+  it 'is invalid without end date' do
+    work_experience = build(:work_experience, end_date: nil)
+    work_experience.valid?
+    expect(work_experience.errors[:end_date]).to include("can't be blank")
+  end
+
+  it 'returns a pretty start date' do 
+    work_experience = build(:work_experience, start_date: '2007-06-22')
+    expect(work_experience.pretty_start_date).to eq('06/22/2007')
+  end
+  it 'returnsa pretty end date' do
+    work_experience = build(:work_experience, end_date: '2009-06-22')
+    expect(work_experience.pretty_end_date).to eq('06/22/2009')
+  end
 end
