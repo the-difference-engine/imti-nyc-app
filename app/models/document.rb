afeef8b2728d8ff2fc1e8dfa7b04b4b19924df93
Josh <jspringxc@gmail.com>
2017-06-26 20:50:08 -0400

    cleaned up should syntax in tests, removed database cleaner, uncommented paperclip validation in document.rb

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,5 +5,5 @@ class Document < ApplicationRecord
   validates_attachment :attachment, content_type: { content_type: ['application/pdf', 'application/msword', 'application/xml', /\Aapplication\/vnd.openxmlformats-.*/, 'image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
   validates :attachment_file_name, presence: true
   validates_uniqueness_of :category, scope:[:application_id]
-  
+
 end
