7991093afe7d7d6461cbec32dd050a1267e1da5b
Dov Bluming <dovybluming@gmail.com>
2017-06-26 22:59:44 -0400

    work_experiences controller

--- spec/factories/work_experience.rb
+++ spec/factories/work_experience.rb
@@ -7,5 +7,9 @@ FactoryGirl.define do
     occupation Faker::Job.title
     start_date '2004-06-22'
     end_date '2016-06-22'
+
+    factory :invalid_work_experience do
+      employer nil
+    end
   end
 end
