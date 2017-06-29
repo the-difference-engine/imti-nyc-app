8cb70a7d2c8c8efcd352ff3b97c5514448eb03eb
Andrew <andrewsouthard111@gmail.com>
2017-06-29 18:41:45 -0400

    Made user factory, refactored course_spec has_many relationship, deleted unused comment

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,8 @@
-FactoryGirl.define do
+FactoryGirl.define do 
   factory :user do 
     first_name "John"
      last_name "Smith"
      email "js@email.com"
-     # course_id course.id
      password "password"
   end 
 end
