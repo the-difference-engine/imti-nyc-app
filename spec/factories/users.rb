a07e9dc8c247e264178eb0ae5507625df2fa56c3
Andrew <andrewsouthard111@gmail.com>
2017-06-29 18:45:31 -0400

    Added faker values to factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do 
   factory :user do 
-    first_name "John"
-     last_name "Smith"
-     email "js@email.com"
-     password "password"
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    email "#{first_name}#{last_name}@email.com"
+    password "password"
   end 
 end
