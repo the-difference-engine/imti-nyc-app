be5e19da946d93cf70b605023def014a375c4d6b
Jamie MacKillop <jamieronin@gmail.com>
2017-06-29 19:29:04 -0400

    Delete coverage and re-add for gitignore

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,8 @@
 class Document < ApplicationRecord
   belongs_to :application
 
-  has_attached_file :attachment, PAPERCLIP_STORAGE_OPTIONS 
+  has_attached_file :attachment
+  # , PAPERCLIP_STORAGE_OPTIONS 
   validates_attachment :attachment, content_type: { content_type: ['application/pdf', 'application/msword', 'application/xml', /\Aapplication\/vnd.openxmlformats-.*/, 'image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
   validates :attachment_file_name, presence: true
   validates_uniqueness_of :category, scope:[:application_id]
