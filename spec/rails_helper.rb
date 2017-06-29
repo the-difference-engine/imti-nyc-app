ee37894f095adbd24a69be0df25672cd1d9a323a
Andrew <andrewsouthard111@gmail.com>
2017-06-29 18:10:36 -0400

    commented out config.fixture_path

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,7 @@ RSpec.configure do |config|
   config.include ApplicationHelper
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
   
   #config for 
 
