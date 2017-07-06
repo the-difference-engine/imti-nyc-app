39a984d872e9ed02d929a99138001be95a2e4bc8
James Chou <jameschou1993@gmail.com>
2017-07-06 19:10:56 -0400

    uncommented paperclip storage and added paperclip storage options to test.rb

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ApplicationRecord
   belongs_to :application
 
   has_attached_file :attachment
-  # , PAPERCLIP_STORAGE_OPTIONS 
+  PAPERCLIP_STORAGE_OPTIONS 
   validates_attachment :attachment, content_type: { content_type: ['application/pdf', 'application/msword', 'application/xml', /\Aapplication\/vnd.openxmlformats-.*/, 'image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
   validates :attachment_file_name, presence: true
 
