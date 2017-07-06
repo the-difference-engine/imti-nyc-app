81b606958bd9e708954afb1ee9f692c8bd8c7adb
James Chou <jameschou1993@gmail.com>
2017-07-06 19:18:21 -0400

    separated user factory to it's own file

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -1,11 +1,4 @@
 FactoryGirl.define do
-  factory :user do
-    email { Faker::Internet.email }
-    password { "password" }
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-  end
-
   factory :document do 
     association :application
     category "health_form"
