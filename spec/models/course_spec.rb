19c84778f2968b3e0fbd74a3d6a6e192f4412c9d
Trevor Jones <trevordavidjones@gmail.com>
2017-07-06 18:37:54 -0400

    Fix merging problems I added

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,33 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  it "has many users" do
-    course = FactoryGirl.build(:course)
-    john = FactoryGirl.build(:user)
-    tom = FactoryGirl.build(:user, first_name: "tom")
+  it "has valid factory" do
+    expect(FactoryGirl.create(:course)).to be_valid
+  end
 
-    course.users << john << tom
-
-    # p "course #{course.id}"
-    # john = User.create!(
-    #   first_name: "John",
-    #   last_name: "Smith",
-    #   email: "js@email.com",
-    #   course_id: course.id,
-    #   password: "password",
-    #   password_confirmation: "password"
-    # )
-    # jeff = User.create!(
-    #   first_name: "Jeff",
-    #   last_name: "Sessions",
-    #   email: "jjj@email.com",
-    #   course_id: course.id,
-    #   password: "password",
-    #   password_confirmation: "password"
-    # )
-    # p "course.users #{course.users}"
-    # p "course.users #{course.users[0]}"
-    expect(course.users[0].id).to eq(john.id)
+  it "price is a decimal" do
+    course = FactoryGirl.create(:course)
+    expect(course.price.class).to eq(BigDecimal)
   end
 
   it "has many users" do
