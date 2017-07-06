523940e222d29ab6c940246a27258558a4d62422
James Chou <jameschou1993@gmail.com>
2017-07-06 18:12:41 -0400

    fixed currrent user error

---
+++ spec/factories/documents.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :documents do
+    category "health_form"
+    attachment_file_name "1099CompositeandYearEndSummary20160210176928.pdf"
+    attachment_file_size 73764
+    attachment_content_type "application/pdf"
+
+    
+  end
+end
