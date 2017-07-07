90686ac526d5a61152347bf50c3a3b28cfd1f1dc
Jamie MacKillop <jamieronin@gmail.com>
2017-07-06 20:17:50 -0400

    Add sessions_controller to handle email to admin for unfinished application. Created MailGun helper in sessions_helper. Make sure to get the MAILGUN API key

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,8 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
   def destroy
-    p "DID I MAKE IT TO THE SESSIONS DESTROY CONTROLLER??????????????????????"
     if Application.where(user_id: current_user.id).count > Charge.where(user_id: current_user.id).count
-      p "****************************************************Send an email"
-      p "Application.where(user_id: current_user.id).count is #{Application.where(user_id: current_user.id).count}"
-      p "Charge.where(user_id: current_user.id).count is #{Charge.where(user_id: current_user.id).count}"
-      
+      send_email_mailgun
     end
     sign_out @user
     redirect_to "/sign_in"
