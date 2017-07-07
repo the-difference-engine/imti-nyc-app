a2fa0f081bf999c26476a34e163d5a6d70bc1dd8
Jamie MacKillop <jamieronin@gmail.com>
2017-07-06 21:00:49 -0400

    Refactor sessions controller to use Devise syntax

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Users::SessionsController < Devise::SessionsController
+  include SessionsHelper
+  before_action :send_admin_email, only: :destroy 
+
+  def send_admin_email
+    current_user.application
+    if !current_user.application.try(:payment_status?)
+      send_email_mailgun
+    end
+  end
+end
