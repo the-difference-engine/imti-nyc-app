c9e1247bcaced570632607c4ad313ec6ffe05cab
Jamie MacKillop <jamieronin@gmail.com>
2017-07-06 20:17:50 -0400

    Add sessions_controller to handle email to admin for unfinished application. Created MailGun helper in sessions_helper. Make sure to get the MAILGUN API key

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+  def send_email_mailgun
+
+    mg_client = Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
+
+    message_params =  {
+                       from: 'imtinyc@gmail.com',
+                       to:   'jamieronin@gmail.com',
+                       subject: "The user #{current_user.first_name} #{current_user.last_name} hasn't finished the application",
+                       text:    "The user #{current_user.first_name} #{current_user.last_name} hasn't finished the application. You can email them at: #{current_user.email}"
+                      }
+
+    result = mg_client.send_message('sandbox4f9920610a894b81b82f6bc37e90f1a0.mailgun.org', message_params).to_h!
+
+    message_id = result['id']
+    message = result['message']
+  end 
 end
