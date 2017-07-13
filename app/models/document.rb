7c75b52a015d5a7c5022237b0ede0fb567ef37b1
michaelyatco <mjyatco@gmail.com>
2017-07-13 19:29:39 -0400

    Only one type of each document can be uploaded in application

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,4 +4,5 @@ class Document < ApplicationRecord
   has_attached_file :attachment, PAPERCLIP_STORAGE_OPTIONS 
   validates_attachment :attachment, content_type: { content_type: ['application/pdf', 'application/msword', 'application/xml', /\Aapplication\/vnd.openxmlformats-.*/, 'image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
   validates :attachment_file_name, presence: true
+  validates_uniqueness_of :category, scope:[:application_id]
 end
