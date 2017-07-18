bbb482a81b67e0292f5f3cdcef85170aa7921dfc
Andrew <andrewsouthard111@gmail.com>
2017-07-17 20:40:21 -0400

    Added routes in workshop_controller payment method, now getting invalid card

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -37,6 +37,42 @@ class WorkshopsController < ApplicationController
     @workshop = Workshop.find_by( id: params[:id]) 
     @workshop.destroy
     redirect_to "/workshops"
+  end
+
+  def payment
+    amount = current_user.domestic_applicant? ? 50 : 75;
+    registrantWorkshop = RegistrantWorkshop.where(workshop_id: params[:id])
+    
+    count = 0
+    registrantWorkshop.each do |registrant_workshop|
+      # where the local school id matches
+      registrant = Registrant.find_by(id: registrant_workshop.registrant_id)
+      if registrant.local_school_id == current_user.id
+        registrant_workshop.update(payment_status: true)
+        count += 1
+      end
+    end
+
+    customer = Stripe::Customer.create(
+      :email => current_user.email,
+      :source  => params[:stripeToken]
+      )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => amount * 100,
+      :description => "Application fee",
+      :currency    => 'usd'
+      )
+
+    new_charge = current_user.charges.create(uid: charge.id, amount: amount, description: charge.description, customer_id: customer.id)
+    
+    flash[:success] = "Thank you for your payment."
+    redirect_to application_path(application.id)
+
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to "/workshops/#{params[:workshop_id]}"
   end  
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717230542) do
+ActiveRecord::Schema.define(version: 20170717234831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,8 +118,9 @@ ActiveRecord::Schema.define(version: 20170717230542) do
   create_table "registrant_workshops", force: :cascade do |t|
     t.integer  "registrant_id"
     t.integer  "workshop_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.boolean  "payment_status"
   end
 
   create_table "registrants", force: :cascade do |t|
