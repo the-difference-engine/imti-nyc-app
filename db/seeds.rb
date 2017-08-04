0c90e9ebd308e6f44b81be4c0f4053de37e6ff15
Marco <mgenova03@gmail.com>
2017-08-03 20:35:47 -0400

    add will_paginate gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do 
+   user = User.new(email:Faker::Internet.email,first_name:Faker::Name.first_name, last_name:Faker::Name.last_name)
+   user.password = "password"
+   user.save
+end 
