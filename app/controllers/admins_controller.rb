065aad7d70cc983164004517741e4fee47d11b42
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-08-10 20:51:36 -0400

    built out routes and controller methods to move to admin and local admin tables

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,10 +5,25 @@ class AdminsController < ApplicationController
     @users_not_admin = User.where('role > 1')
     render 'show_users'
   end
+
   def move_to_admin
+    user = User.find_by(id: params[:user_id])
+    user.role = "admin"
+    user.save
+    redirect_to :back
   end
-  def move_to_local_admin
+
+  def move_to_local_school_admin
+    user = User.find_by(id: params[:user_id])
+    user.role = "local_school_admin"
+    user.save
+    redirect_to :back
   end
+
   def remove_admin
+    #delete user
+    user = User.find_by(id: params[:user_id])
+    user.destroy
+    redirect_to :back
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,4 +39,6 @@ Rails.application.routes.draw do
   resources :course_registrations
 
   get '/show_users', to: 'admins#show_users'
+  get '/move_to_admin', to: 'admins#move_to_admin'
+  get '/move_to_local_school_admin', to: 'admins#move_to_local_school_admin'
 end
