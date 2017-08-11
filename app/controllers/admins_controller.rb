29efa79cf3415e5976dfc11125ab4de9b52aad6f
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-08-10 20:05:48 -0400

    built out three tables for admin, local admin and other users.  Created test date for all three.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,8 @@
 class AdminsController < ApplicationController
   def show_users
-    @users = User.all
+    @users_admin = User.where(role: :admin)
+    @users_local_school_admin = User.where(role: :local_school_admin)
+    @users_not_admin = User.where('role > 1')
     render 'show_users'
   end
   def move_to_admin
