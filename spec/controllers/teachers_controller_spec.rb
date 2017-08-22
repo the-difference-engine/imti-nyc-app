49b61609c4da267bc8b36baf939e5c9a02cf4a23
Andrew <andrewsouthard111@gmail.com>
2017-08-22 16:32:57 -0400

    Add test for current users with accepted/not accepted application status

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe TeachersController, type: :controller do
-
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    before :each do
+      @app = create(:application)
+      @user = sign_in create(:user)
+    end
+
+    context "user is accepted" do
+      it "renders index" do
+        controller.current_user.application = @app
+        @app.application_status = "accepted"
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+    context "user is rejected" do
+      it "redirects to pages#index" do
+        controller.current_user.application = @app
+        get :index
+        expect(response).to redirect_to "/"
+      end
     end
   end
-
 end
