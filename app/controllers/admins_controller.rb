29fb1e9d94905bb0e50f812ea998888d8a1a1aa9
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-08-24 20:56:51 -0400

    first commit

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -9,4 +9,23 @@ class AdminsController < ApplicationController
 
     redirect_to "/completed_applications"
   end
+  
+
+  def new
+    
+  end
+
+  def assign_current_teacher
+    application = Application.find_by(id: params[:application_id])
+    p "$"* 50
+    p params[:status]
+    application.update(application_status: params[:status])
+
+    if params[:status] == "approved"
+      application.user.update(role: "current_teacher")
+    end
+
+    redirect_to "/completed_applications"
+  end
+
 end
