96a7a8a4ea36e989d98cb423d9614a311ddd6293
Jamie MacKillop <jamieronin@gmail.com>
2017-08-24 20:23:08 -0400

    Add /courses link to main nav. Add space to stripe.js.erb to allow credit card payment module to show up - bug fix. Add logic to course registrations controller to save a new instance of user_courses if a stripe charge is successfully saved apon payment. Fix workshop controller bug by adding registrant workshop, count, and @amount to the show action.

--- app/controllers/course_registrations_controller.rb
+++ app/controllers/course_registrations_controller.rb
@@ -4,8 +4,8 @@ class CourseRegistrationsController < ApplicationController
   end
 
   def create
-    course = Course.find(course_params[:course_id])
-    amount = (course.price * 100).to_i
+    @course = Course.find(course_params[:course_id])
+    amount = (@course.price * 100).to_i
     customer = Stripe::Customer.create(
       :email => current_user.email,
       :source  => params[:stripeToken]
@@ -14,12 +14,16 @@ class CourseRegistrationsController < ApplicationController
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => amount,
-      :description => course.name,
+      :description => @course.name,
       :currency    => 'usd'
       )
 
-    @new_charge = current_user.charges.create(uid: charge.id, amount: charge.amount / 100, description: charge.description, customer_id: customer.id)
-    current_user.update(course_id: course.id)
+    @new_charge = current_user.charges.new(uid: charge.id, amount: charge.amount / 100, description: charge.description, customer_id: customer.id)
+    if @new_charge.save 
+      UserCourse.create(user_id: current_user.id, course_id: @course.id, payment_status: "accepted")
+    end
+    # 
+    # current_user.update(course_id: course.id)
     render 'show'
 
   rescue Stripe::CardError => e
