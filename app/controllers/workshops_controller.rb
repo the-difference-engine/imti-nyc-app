49524d4f69cf3639abdef9efeb3702f90479c94e
Jamie MacKillop <jamieronin@gmail.com>
2017-08-28 20:19:58 -0400

    Delete spaces from stripe.js.erb to fix credit card modal bug. Fix bug with workshop controller to allow local schools and single users to pay for workshops. Add links to application.html.erb toworkshops and courses index. Fix bug with workshop show to check if local school admin

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -19,7 +19,15 @@ class WorkshopsController < ApplicationController
   def show
     @workshop = Workshop.find_by(id: params[:id])
     @registrant = Registrant.new
-    @registrants = @workshop.registrants.where(local_school_id: current_user.local_school_id) if current_user.local_school_admin? 
+    @registrants = @workshop.registrants.where(local_school_id: current_user.local_school_id) if current_user.local_school_admin?
+    if current_user.local_school_admin?
+      registrant_workshop = RegistrantWorkshop.where(workshop_id: params[:id])
+      count = calc_count_in_show(registrant_workshop)
+      # amount should be changed to workshop price multiplied by count
+      @amount = count * @workshop.price.to_i
+    elsif @workshop
+      @amount = @workshop.price.to_i
+    end 
     render 'show.html.erb'  
   end
 
@@ -46,7 +54,7 @@ class WorkshopsController < ApplicationController
   
     count = calc_count_in_payment(registrant_workshop)
     # amount should be changed to workshop price multiplied by count
-    @amount = count * @workshop.price.to_i ;
+    @amount = count * @workshop.price.to_i
     # binding.pry
     puts "=" *100
     puts "#{@amount} @amount" 
