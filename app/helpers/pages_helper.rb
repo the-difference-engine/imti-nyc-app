ac25da1792c5c92f041ecdf3d961284f89da0d75
Jamie MacKillop <jamieronin@gmail.com>
2017-08-31 20:59:47 -0400

    Added send confirmation email to contact page in order for senders to recieve a confirmation
    Will need to replace the recipient to sender's email once in production as our mailgun domain is currently sandbox.....

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,22 +1,35 @@
 module PagesHelper
   def send_email_mailgun
     mg_client = Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
-
     message_params = {
-       from: 'imtinyc@gmail.com',
-       to:   'dovybluming@gmail.com',
-       subject: params[:subject],
-       html:  "
-         <p>Name: #{params[:name]}</p>
-         <p>Email: #{params[:email]}</p>
-         <p>Phone Number: #{params[:number]}</p>
-         <h3>Message</h3>
-         <p>#{params[:message]}</p>"
-      }
+      from: 'imtinyc@gmail.com',
+      to:   'jamieronin@gmail.com',
+      subject: params[:subject],
+      html:  "
+      <p>Name: #{params[:name]}</p>
+      <p>Email: #{params[:email]}</p>
+      <p>Phone Number: #{params[:number]}</p>
+      <h3>Message</h3>
+      <p>#{params[:message]}</p>"
+    }
 
-    result = mg_client.send_message('sandbox4f9920610a894b81b82f6bc37e90f1a0.mailgun.org', message_params).to_h!
+    mg_client.send_message('sandbox4f9920610a894b81b82f6bc37e90f1a0.mailgun.org', message_params).to_h!
+  end
 
-    message_id = result['id']
-    message = result['message']
+  def send_email_confirmation
+    mg_client = Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
+    message_params = {
+      from: 'imtinyc@gmail.com',
+      #  Will replace to: email to params[:email] once in production
+      to:   'jamieronin@gmail.com',
+      subject: params[:subject],
+      html:  "
+      <p>Hello #{params[:name]}</p>
+      <p>Your message has been successfully been sent to IMTI!</p>
+      <h3>Message</h3>
+      <p>#{params[:message]}</p>"
+    }
+    #  need to change sandbox to production to allow emails to be sent to external email addresses
+    mg_client.send_message('sandbox4f9920610a894b81b82f6bc37e90f1a0.mailgun.org', message_params).to_h!
   end
 end
