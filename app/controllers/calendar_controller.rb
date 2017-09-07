d237a15df3fc137b72b77e23e884ce05c4daefc6
j-eaves <jdeaves@gmail.com>
2017-09-07 19:38:21 -0400

    changed code on calendar show page to make room for calendar expansion

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -15,16 +15,17 @@ class CalendarController < ApplicationController
 		@month = "#{params[:date]["month"]}"
 		@year = "#{params[:date]["year"]}"
 		@date = "#{day}"+"/"+"#{month}"+"/"+"#{year}"
-		strp_format = "%d%m%Y"
+		# strp_format = "%d%m%Y"
+		strp_format = "%@day%@month%@year"
 		start_date = DateTime.strptime(date, strp_format)
 
 		Event.create(
 			title: params[:title],
 			location: params[:location],
-			start_date: start_date,
-			end_date: start_date,
+			# start_date: start_date,
+			# end_date: start_date,
 			# CHANGE THIS TO END_DATE
-			details: params[:details],
+			# details: params[:details],
 		)
 		redirect_to '/calendar#show'
 	end
