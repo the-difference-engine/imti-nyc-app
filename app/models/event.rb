b9bdf1321d2f4467b7f2cd9b0326b8b3d51e9ac2
j-eaves <jdeaves@gmail.com>
2017-09-11 18:29:08 -0400

    changed code on calendar show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,7 @@
 class Event < ApplicationRecord
-	def pretty_date_start
-		start_time.strftime("%I:%M %p")
-	end
-	def pretty_date_end
-		end_time.strftime("%I:%M %p")
+	def pretty_date(date)
+		if date
+			date.strftime("%I:%M %p")
+		end
 	end
 end
