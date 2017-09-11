21dd90cbdc53e6d0b4541bbc2a80b731fc46f056
Janice Wong <go.janicewong@gmail.com>
2017-09-11 19:48:29 -0400

    Add create event form

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,33 +1,28 @@
 class CalendarController < ApplicationController
   skip_before_action :authenticate_user!
 
-	def show
-	    @title = "Calendar"
-	    @events = Event.all
-		render 'show.html.erb'
-	end
-	def new_event
-		render 'new'
-	end
-	def create
-		# date = "#{params[:date]["day"]}/#{params[:date]["month"]}/#{params[:date]["year"]}"
-		@day = "#{params[:date]["day"]}"
-		@month = "#{params[:date]["month"]}"
-		@year = "#{params[:date]["year"]}"
-		@date = "#{day}"+"/"+"#{month}"+"/"+"#{year}"
-		# strp_format = "%d%m%Y"
-		strp_format = "%@day%@month%@year"
-		# strp_format = "%A, %d %b %Y %l:%M %p"
-		start_date = DateTime.strptime(date, strp_format)
-		end_date = DateTime.strptime(date, strp_format)
-		Event.create(
-			title: params[:title],
-			location: params[:location],
-			start_date: start_date,
-			end_date: start_date,
-			# CHANGE THIS TO END_DATE
-			details: params[:details],
-		)
-		redirect_to '/calendar#show'
-	end
+  def show
+    @title = "Calendar"
+    @events = Event.all
+    render 'show.html.erb'
+  end
+
+  def new_event
+    render 'new'
+  end
+
+  def create
+    date = "#{params[:date]['month']}/#{params[:date]['day']}/#{params[:date]['year']}"
+    strp_format = "%m/%d/%Y"
+    start_time = DateTime.strptime(date, strp_format)
+    Event.create(
+      title: params[:title],
+      location: params[:location],
+      start_time: start_time,
+      # once we figure out how to add multi-day events, we can update end_time
+      end_time: start_time,
+      details: params[:details]
+    )
+    redirect_to '/calendar'
+  end
 end
