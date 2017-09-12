9911d561f16b8caa85fbe3f690719862137c8786
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-09-11 21:58:33 -0400

    touchups' '

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -16,13 +16,5 @@ module AdminsHelper
     message = result['message']
   end
 
-  def self.send_mailgun_2
-      RestClient.post "https://api:key-e9e94aca9f475a71df00dae854da9866@api.mailgun.net/v3/sandbox4f9920610a894b81b82f6bc37e90f1a0.mailgun.org/messages",
-      :from => "Excited User <mailgun@sandbox4f9920610a894b81b82f6bc37e90f1a0.mailgun.org>",
-      :to => "jordanhiattcodes@gmail.com",
-      :subject => "Hello",
-      :text => "Testing some Mailgun awesomness!"
-  end
-end
 
 
