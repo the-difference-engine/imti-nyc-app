2a0a74504e7bcadd076118bd0c97ff5cda14842c
Dov Bluming <dovybluming@gmail.com>
2017-09-14 19:27:16 -0400

    added current_job to work_experience table

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -3,11 +3,4 @@ class WorkExperience < ApplicationRecord
   validates :end_date, presence: true, :unless => :current_job?
 	belongs_to :application
   
-  def current_job(answer)
-    @answer = answer == 'on' ? true : false
-  end
-
-  def current_job?
-    @answer
-  end 
 end
