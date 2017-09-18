f1cd3f7a785c2d9f1f3a75c161e9a401e5523940
j-eaves <jdeaves@gmail.com>
2017-09-18 18:57:31 -0400

    created calendar_events controller and accompanying create page and routes

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :event do
     title {Faker::Company.catch_phrase}	
     location {Faker::GameOfThrones.city}
-    start_time {Faker::Time.between(DateTime.now - 1, DateTime.now)}
-    end_time {Faker::Time.between(DateTime.now + 1, DateTime.now)}
+    start_time {Faker::Time.between(DateTime.now + 1, DateTime.now + 2)}
+    end_time {Faker::Time.between(DateTime.now + 2, DateTime.now + 3)}
     details {Faker::Company.bs}
   end
 end
