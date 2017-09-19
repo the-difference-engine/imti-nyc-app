b1d008c4ef57994cb2ef93c2c8def5b536f52ca8
j-eaves <jdeaves@gmail.com>
2017-09-18 21:00:04 -0400

    Clean up chagnes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,9 @@
 class Event < ApplicationRecord
-<<<<<<< 1dbbe9c6d227b832b4e83bcb217be9d04ebfd45b
   validates :title, :start_time, presence: true
 
 	def pretty_time(time)
 		if time
 			time.strftime("%I:%M %p")
+		end
 	end
-	end
-=======
->>>>>>> added code in factory girl to populate events in calendar
 end
