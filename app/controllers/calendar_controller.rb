14da4dc8f1972daaf48099a9e80657c010c0da25
j-eaves <jdeaves@gmail.com>
2017-09-18 21:00:04 -0400

    Clean up chagnes

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -6,19 +6,4 @@ class CalendarController < ApplicationController
     @events = Event.all
     render 'show.html.erb'
   end
-
-  def create
-    date = "#{params[:date]['month']}/#{params[:date]['day']}/#{params[:date]['year']}"
-    strp_format = "%m/%d/%Y"
-    start_time = DateTime.strptime(date, strp_format)
-    Event.create(
-      title: params[:title],
-      location: params[:location],
-      start_time: start_time,
-      # once we figure out how to add multi-day events, we can update end_time
-      end_time: start_time,
-      details: params[:details]
-    )
-    redirect_to '/calendar'
-  end
 end
