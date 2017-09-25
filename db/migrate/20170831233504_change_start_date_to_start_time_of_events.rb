28e78000366e0b3d4ca3b41878a8c9014da477ef
j-eaves <jdeaves@gmail.com>
2017-09-25 18:55:14 -0400

    Roll back and change calendar_event model

--- db/migrate/20170831233504_change_start_date_to_start_time_of_events.rb
+++
@@ -1,6 +0,0 @@
-class ChangeStartDateToStartTimeOfEvents < ActiveRecord::Migration[5.0]
-  def change
-  	rename_column :events, :start_date, :start_time
-  	rename_column :events, :end_date, :end_time
-  end
-end
