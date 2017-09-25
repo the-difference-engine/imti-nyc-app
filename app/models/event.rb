ab738d26e93e0fcb5a1e3b9588b7c17a41482e0b
j-eaves <jdeaves@gmail.com>
2017-09-25 18:55:14 -0400

    Roll back and change calendar_event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,10 @@
 class Event < ApplicationRecord
   validates :title, :start_time, presence: true
 
-	def pretty_time(time)
-		if time
-			time.strftime("%I:%M %p")
-		end
-	end
+  def pretty_time(time)
+    if time
+    	time.strftime("%I:%M %p")
+    end
+  end
 end
+  
