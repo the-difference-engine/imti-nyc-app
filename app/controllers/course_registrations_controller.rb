ecbe3fe19811ea223868473283fe097af6150934
Marco <mgenova03@gmail.com>
2017-09-25 20:16:44 -0400

    add email confirmation to course registation

--- app/controllers/course_registrations_controller.rb
+++ app/controllers/course_registrations_controller.rb
@@ -1,11 +1,13 @@
 class CourseRegistrationsController < ApplicationController
+  include CourseRegistrationsHelper
+
   def new
     @courses = Course.all
   end
 
   def create
-    course = Course.find(course_params[:course_id])
-    amount = (course.price * 100).to_i
+    @course = Course.find(course_params[:course_id])
+    amount = (@course.price * 100).to_i
     customer = Stripe::Customer.create(
       :email => current_user.email,
       :source  => params[:stripeToken]
@@ -14,12 +16,13 @@ class CourseRegistrationsController < ApplicationController
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => amount,
-      :description => course.name,
+      :description => @course.name,
       :currency    => 'usd'
       )
 
     @new_charge = current_user.charges.create(uid: charge.id, amount: charge.amount / 100, description: charge.description, customer_id: customer.id)
     current_user.update(course_id: course.id)
+    send_email_mailgun
     render 'show'
 
   rescue Stripe::CardError => e
--- app/helpers/course_registrations_helper.rb
+++ app/helpers/course_registrations_helper.rb
@@ -1,2 +1,18 @@
 module CourseRegistrationsHelper
+  def send_email_mailgun
+    mg_client = Mailgun::Client.new(ENV['MAILGUN_API_KEY'])
+    message_params = {
+      from: 'imtinyc@gmail.com',
+      to:   current_user.email,
+      subject: 'Course Confirmation',
+      html:  "
+      <p> Hi #{current_user.first_name} #{current_user.last_name} , </p>
+
+      <p>This is a Confirmation that you have signed up for <strong>#{@course.name}</strong> course</p>
+
+   "
+    }
+
+    mg_client.send_message('sandbox4f9920610a894b81b82f6bc37e90f1a0.mailgun.org', message_params).to_h!
+  end
 end
