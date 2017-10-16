2db9885f074bed20ff54d3384a9dd948871f8c1d
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-10-16 19:02:54 -0400

    added view authorizations to the cancancan gem

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,8 +1,5 @@
 class TeachersController < ApplicationController
   def index
-    if current_user.application.application_status != "accepted"
-      flash[:danger] = "Your application must first be accepted in order to access this page."
-      redirect_to "/"
-    end
+
   end
 end
