c87d89e5c837e760773707ef4542f73a7109a4cc
Josh <jspringxc@gmail.com>
2017-10-23 20:04:13 -0400

    checked for current user in controller before auto-filling user info

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -1,6 +1,6 @@
 class WorkshopsController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def index
     @workshops = Workshop.all
     render 'index.html.erb'
@@ -18,6 +18,7 @@ class WorkshopsController < ApplicationController
 
   def show
     @workshop = Workshop.find_by(id: params[:id])
+    @user = current_user.presence || User.new
     @registrant = Registrant.new
     @registrants = @workshop.registrants.where(local_school_id: current_user.local_school_id) if current_user.local_school_admin?
     if current_user.local_school_admin?
@@ -27,11 +28,11 @@ class WorkshopsController < ApplicationController
       @amount = count * @workshop.price.to_i
     elsif @workshop
       @amount = @workshop.price.to_i
-    end 
-    render 'show.html.erb'  
+    end
+    render 'show.html.erb'
   end
 
-  def edit 
+  def edit
     @workshop = Workshop.find_by(id: params[:id])
     render 'edit.html.erb'
   end
@@ -42,22 +43,22 @@ class WorkshopsController < ApplicationController
     redirect_to "/workshops/#{@workshop.id}"
   end
 
-  def destroy 
-    @workshop = Workshop.find_by( id: params[:id]) 
+  def destroy
+    @workshop = Workshop.find_by(id: params[:id])
     @workshop.destroy
     redirect_to "/workshops"
   end
 
   def payment
-    registrant_workshop = RegistrantWorkshop.where(workshop_id: params[:workshop_id] )
+    registrant_workshop = RegistrantWorkshop.where(workshop_id: params[:workshop_id])
     @workshop = Workshop.find_by(id: params[:workshop_id])
-  
+
     count = calc_count_in_payment(registrant_workshop)
     # amount should be changed to workshop price multiplied by count
     @amount = count * @workshop.price.to_i
     # binding.pry
-    puts "=" *100
-    puts "#{@amount} @amount" 
+    puts "=" * 100
+    puts "#{@amount} @amount"
     puts "#{count} count"
     puts "#{@workshop.price.to_i} workshoppirce"
     customer = Stripe::Customer.create(
@@ -72,35 +73,35 @@ class WorkshopsController < ApplicationController
       :currency    => 'usd'
       )
     new_charge = current_user.charges.create(uid: charge.id, amount: @amount, description: charge.description, customer_id: customer.id)
-    
+
     flash[:success] = "Thank you for your payment."
     redirect_to workshop_path(params[:workshop_id])
 
     rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to "/workshops/#{params[:workshop_id]}"
-  end  
+  end
 
   def calc_count_in_payment(registrant_workshops)
     count = 0
-    puts "calc count" + "=="*100 
+    puts "calc count" + "==" * 100
     # checks if current user is local school admin
     if current_user.local_school_id
-      puts "local school?" + "=="*100 
+      puts "local school?" + "==" * 100
       registrant_workshops.each do |registrant_workshop|
         # where the local school id matches
         registrant = Registrant.find_by(id: registrant_workshop.registrant_id)
         # counts number of registrants from current user's local school that have a payment status of false
 
-          # puts registrant_workshop.payment_status + "=="*100 
-        if registrant.local_school_id == current_user.local_school_id && !registrant_workshop.payment_status 
-          puts "=="*100
+        # puts registrant_workshop.payment_status + "=="*100
+        if registrant.local_school_id == current_user.local_school_id && !registrant_workshop.payment_status
+          puts "==" * 100
           registrant_workshop.update(payment_status: true)
           count += 1
         end
       end
       # current user is not affiliated with local school
-    else 
+    else
       count += 1
     end
     count
@@ -118,7 +119,7 @@ class WorkshopsController < ApplicationController
           count += 1
         end
       end
-    else 
+    else
       count += 1
     end
     count
