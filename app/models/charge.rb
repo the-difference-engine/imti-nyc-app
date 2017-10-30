1335d3b78014165ef8ced0602a3cadda3bc17681
Trevor Jones <trevordavidjones@gmail.com>
2017-10-30 19:51:54 -0400

    Send confirmation email
    Also display confirmation number

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -2,5 +2,5 @@ class Charge < ApplicationRecord
   include ActionView::Helpers::NumberHelper
   belongs_to :user, optional: true
   validates :uid, :amount, :customer_id, :charge_type, presence: true
-  validates :charge_type, inclusion: { in: %w(application course donation  workshop) }
+  validates :charge_type, inclusion: { in: %w(application course donation workshop) }
 end
