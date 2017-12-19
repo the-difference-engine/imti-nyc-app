92366e8ed83037514a553578912daf5b151a03d6
Trevor Jones <trevordavidjones@gmail.com>
2017-12-18 20:34:21 -0500

    Removed unused content

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   validates :title, :start_time, presence: true
 
+  scope :upcoming_events, -> { where('start_time > ?', Time.current).limit(3) }
+  
   def pretty_time(time)
     if time
     	time.strftime("%I:%M %p")
